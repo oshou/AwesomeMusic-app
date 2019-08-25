bd1f104714317ac6989b0ffd2c460a0c5ef01a2e
oshou <adf1985adf@gmail.com>
2019-08-25 13:54:38 +0900

    add api_url config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,9 @@ export default {
   css: [{ src: "@/node_modules/uikit/dist/css/uikit.css" }],
   plugins: [{ src: "@/plugins/uikit.js", ssr: false }, "@/plugins/auth0.js"],
   modules: ["@nuxtjs/axios"],
-  axios: {},
+  axios: {
+    baseUrl: process.env.BASE_URL || "http://localhost:3000"
+  },
   auth0: {
     domain: "awesome-music.auth0.com",
     clientID: "ReBHBsldURl6lQhETGg5SWOU95et9Rw8"
