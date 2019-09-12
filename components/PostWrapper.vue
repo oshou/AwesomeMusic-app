a749360678174c5d6859ac84e1893f3d10c1b978
oshou <adf1985adf@gmail.com>
2019-09-12 09:29:11 +0900

    重複記述が多いので投稿一覧をコンポーネント化

---
+++ components/PostWrapper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="uk-container-expand" id="latest">
+    <div uk-grid class="uk-child-width-1-2">
+      <div v-for="post in $store.getters['posts/getPosts']" v-bind:key="post.id">
+        <div class="uk-card uk-card-small uk-card-default uk-column-span">
+          <div class="uk-card-header">
+            <div class="uk-grid-small uk-flex-middle" uk-grid>
+              <div class="uk-width-auto">
+                <span uk-icon="icon: play-circle; ratio: 1.8"></span>
+              </div>
+              <div class="uk-width-expand">
+                <h4 class="uk-card-title uk-margin-remove-bottom">{{post.title}}</h4>
+                <p class="uk-text-meta uk-margin-remove-top">{{ post.url }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="uk-card-body">
+            <p>{{ post.message }}</p>
+          </div>
+          <div class="uk-card-footer">
+            <div uk-grid>
+              <div class="uk-width-auto">
+                <div v-for="tag in $store.getters['tags/getTags']" v-bind:key="tag.id">
+                  <a :href="`/tags/${tag.id}`">{{tag.name}}</a> /
+                </div>
+              </div>
+              <div class="uk-width-expand">
+                <nuxt-link v-bind:to="{name: 'posts-id-comments',params:{id:post.id}}">
+                  <span uk-icon="icon: comments; ratio: 1.0"></span>
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.uk-card {
+  border-radius: 20px;
+}
+</style>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {};
+</script>
--- pages/tags/_id.vue
+++ pages/tags/_id.vue
@@ -1,51 +1,19 @@
 <template>
-  <div class="uk-container-expand" id="posts">
-    <div uk-grid class="uk-child-width-expand">
+  <div id="container">
+    <div uk-grid class="uk-margin-small-bottom">
       <h2>タグ: {{ $store.getters['tags/getTags'].name }} の投稿</h2>
     </div>
-    <div uk-grid class="uk-child-width-1-2@s">
-      <div v-for="post in $store.getters['posts/getPosts']" v-bind:key="post.id">
-        <div class="uk-card uk-card-small uk-card-default uk-column-span">
-          <div class="uk-card-header">
-            <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-auto">
-                <span uk-icon="icon: play-circle; ratio: 1.8"></span>
-              </div>
-              <div class="uk-width-expand">
-                <h4 class="uk-card-title uk-margin-remove-bottom">{{post.title}}</h4>
-                <p class="uk-text-meta uk-margin-remove-top">{{ post.url }}</p>
-              </div>
-            </div>
-          </div>
-          <div class="uk-card-body">
-            <p>{{ post.message }}</p>
-          </div>
-          <div class="uk-card-footer">
-            <div uk-grid>
-              <div class="uk-width-auto">
-                <p>xxx / xxx / xxx</p>
-              </div>
-              <div class="uk-width-expand">
-                <nuxt-link v-bind:to="{name: 'posts-id-comments',params:{id:post.id}}">
-                  <span uk-icon="icon: comments; ratio: 1.0"></span>
-                </nuxt-link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PostWrapper />
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import PostWrapper from "@/components/PostWrapper";
 
 export default {
-  data: function() {
-    return {
-      posts: []
-    };
+  components: {
+    PostWrapper
   },
   created() {
     this.fetchPostsByTagId(this.$route.params.id);
